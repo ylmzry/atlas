0aae39ae80d86b453ab15f098930314a137b920d
bananenfisch <markus@bananenfisch.net>
2018-04-04 21:02:22 +0200

    test

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Products extends CI_Controller{
+class Prodxucts extends CI_Controller{
 
   public function __construct() {
       parent::__construct();
