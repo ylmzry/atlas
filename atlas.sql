0b44fa51377aa3880b1cf516f9f1f669115f049e
Eray YILMAZ <erayyilmaz@outlook.com>
2018-04-09 00:28:52 +0200

    sql

--- atlas.sql
+++ atlas.sql
@@ -2,10 +2,10 @@
 -- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Erstellungszeit: 04. Apr 2018 um 18:40
--- Server-Version: 10.1.28-MariaDB
--- PHP-Version: 7.1.10
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 08, 2018 at 10:27 PM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,30 +19,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `atlas`
+-- Database: `atlas`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `activity`
+-- Table structure for table `activity`
 --
 
-CREATE TABLE `activity` (
-  `ActivityID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE IF NOT EXISTS `activity` (
+  `ActivityID` int(11) NOT NULL AUTO_INCREMENT,
   `CustomerID` int(11) NOT NULL,
   `Title` text NOT NULL,
-  `Content` mediumblob NOT NULL
+  `Content` mediumblob NOT NULL,
+  PRIMARY KEY (`ActivityID`),
+  KEY `CustomerID` (`CustomerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `customer`
+-- Table structure for table `customer`
 --
 
-CREATE TABLE `customer` (
-  `CustomerID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` (
+  `CustomerID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(200) NOT NULL,
   `Surname` varchar(200) NOT NULL,
   `Company` varchar(200) NOT NULL,
@@ -54,23 +58,26 @@ CREATE TABLE `customer` (
   `Address` varchar(200) NOT NULL,
   `Address2` varchar(200) NOT NULL,
   `Address3` varchar(200) NOT NULL,
-  `RegistrationDate` date NOT NULL
+  `RegistrationDate` date NOT NULL,
+  PRIMARY KEY (`CustomerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `groups`
+-- Table structure for table `groups`
 --
 
-CREATE TABLE `groups` (
-  `id` mediumint(8) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
-  `description` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `groups`
+-- Dumping data for table `groups`
 --
 
 INSERT INTO `groups` (`id`, `name`, `description`) VALUES
@@ -80,115 +87,138 @@ INSERT INTO `groups` (`id`, `name`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `login_attempts`
+-- Table structure for table `login_attempts`
 --
 
-CREATE TABLE `login_attempts` (
-  `id` int(11) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `login_attempts`;
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(45) NOT NULL,
   `login` varchar(100) NOT NULL,
-  `time` int(11) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `time` int(11) UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `order`
+-- Table structure for table `order`
 --
 
-CREATE TABLE `order` (
-  `OrderID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `OrderID` int(11) NOT NULL AUTO_INCREMENT,
   `CustomerID` int(11) NOT NULL,
   `OrderStatusID` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
-  `Date` date NOT NULL
+  `OrderDate` date NOT NULL,
+  PRIMARY KEY (`OrderID`),
+  KEY `OrderStatusID` (`OrderStatusID`),
+  KEY `CustomerID` (`CustomerID`),
+  KEY `UserID` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `orderline`
+-- Table structure for table `orderline`
 --
 
-CREATE TABLE `orderline` (
-  `OrderLineID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `orderline`;
+CREATE TABLE IF NOT EXISTS `orderline` (
+  `OrderLineID` int(11) NOT NULL AUTO_INCREMENT,
   `OrderID` int(11) NOT NULL,
   `ProductID` int(11) NOT NULL,
   `Quantity` int(11) NOT NULL,
-  `TotalAmount` decimal(10,0) NOT NULL
+  `TotalAmount` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`OrderLineID`),
+  KEY `OrderID` (`OrderID`),
+  KEY `ProductID` (`ProductID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `orderstatus`
+-- Table structure for table `orderstatus`
 --
 
-CREATE TABLE `orderstatus` (
-  `OrderStatusID` int(11) NOT NULL,
-  `Name` varchar(200) NOT NULL
+DROP TABLE IF EXISTS `orderstatus`;
+CREATE TABLE IF NOT EXISTS `orderstatus` (
+  `OrderStatusID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  PRIMARY KEY (`OrderStatusID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `product`
+-- Table structure for table `product`
 --
 
-CREATE TABLE `product` (
-  `ProductID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `ProductID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(200) NOT NULL,
   `Category` varchar(200) NOT NULL,
-  `Price` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Price` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`ProductID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
--- Daten für Tabelle `product`
+-- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`ProductID`, `Name`, `Category`, `Price`) VALUES
-(1, 'Product 1', 'Test', '13'),
-(3, 'Product 2', 'Category', '12'),
-(4, 'Product 3', 'Category 2', '12'),
-(5, 'Product 4', 'Category Name', '12'),
-(6, 'Product 5', 'Category', '12');
+(1, 'HUAWEI MateBook X ', 'Smart Phones', '120'),
+(2, 'Microsoft Surface Laptop', 'Notebook', '1276'),
+(3, 'Lenovo IdeaPad 520s ', 'Notebook', '849'),
+(4, 'Huawei MateBook E', 'Notebook', '1119'),
+(5, 'Samsung Galaxy Note 8', 'Smart Phones', '999'),
+(6, 'Samsung Galaxy S8', 'Smart Phones', '899'),
+(7, 'Samsung Galaxy S8+', 'Smart Phones', '5');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `role`
+-- Table structure for table `role`
 --
 
-CREATE TABLE `role` (
-  `RoleID` int(11) NOT NULL,
-  `RoleType` varchar(200) NOT NULL
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `RoleID` int(11) NOT NULL AUTO_INCREMENT,
+  `RoleType` varchar(200) NOT NULL,
+  PRIMARY KEY (`RoleID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `user`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `UserID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `UserID` int(11) NOT NULL AUTO_INCREMENT,
   `Username` varchar(200) NOT NULL,
   `Password` varchar(200) NOT NULL,
   `FirstName` varchar(200) NOT NULL,
   `Lastname` varchar(200) NOT NULL,
   `Email` varchar(200) NOT NULL,
   `RoleID` int(11) NOT NULL,
-  `ProfileImageUrl` text NOT NULL
+  `ProfileImageUrl` text NOT NULL,
+  PRIMARY KEY (`UserID`),
+  KEY `RoleID` (`RoleID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(45) NOT NULL,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
@@ -204,210 +234,56 @@ CREATE TABLE `users` (
   `first_name` varchar(50) DEFAULT NULL,
   `last_name` varchar(50) DEFAULT NULL,
   `company` varchar(100) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
-(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1521483869, 1, 'Admin', 'istrator', 'ADMIN', '0');
+(1, '127.0.0.1', 'administrator', '$2y$08$lSKMVeYM7WKOg9UHdIfuE.h7UvcbQQ4B/gxH6rEG4gcG9OsIJ92G6', '', 'admin@admin.com', '', NULL, NULL, NULL, 1268889823, 1523208197, 1, 'Admin', 'istrator', 'ADMIN', '0'),
+(2, '::1', 'erayyilmaz@outlook.com', '$2y$08$NEsymqNhsU13SoI0mwqfIuEP6wUs05vgvgRfTfPfeprY3k8l.Vuxq', NULL, 'erayyilmaz@outlook.com', NULL, NULL, NULL, NULL, 1523196519, NULL, 1, 'Eray', 'Yilmaz', 'Atlas', '6766494269');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `users_groups`
+-- Table structure for table `users_groups`
 --
 
-CREATE TABLE `users_groups` (
-  `id` int(11) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `users_groups`;
+CREATE TABLE IF NOT EXISTS `users_groups` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` int(11) UNSIGNED NOT NULL,
-  `group_id` mediumint(8) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `group_id` mediumint(8) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  KEY `fk_users_groups_users1_idx` (`user_id`),
+  KEY `fk_users_groups_groups1_idx` (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
--- Daten für Tabelle `users_groups`
+-- Dumping data for table `users_groups`
 --
 
 INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
 (1, 1, 1),
-(2, 1, 2);
+(2, 1, 2),
+(3, 2, 2);
 
 --
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `activity`
---
-ALTER TABLE `activity`
-  ADD PRIMARY KEY (`ActivityID`),
-  ADD KEY `CustomerID` (`CustomerID`);
-
---
--- Indizes für die Tabelle `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`CustomerID`);
-
---
--- Indizes für die Tabelle `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indizes für die Tabelle `login_attempts`
---
-ALTER TABLE `login_attempts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indizes für die Tabelle `order`
---
-ALTER TABLE `order`
-  ADD PRIMARY KEY (`OrderID`),
-  ADD KEY `OrderStatusID` (`OrderStatusID`),
-  ADD KEY `CustomerID` (`CustomerID`),
-  ADD KEY `UserID` (`UserID`);
-
---
--- Indizes für die Tabelle `orderline`
---
-ALTER TABLE `orderline`
-  ADD PRIMARY KEY (`OrderLineID`),
-  ADD KEY `OrderID` (`OrderID`),
-  ADD KEY `ProductID` (`ProductID`);
-
---
--- Indizes für die Tabelle `orderstatus`
---
-ALTER TABLE `orderstatus`
-  ADD PRIMARY KEY (`OrderStatusID`);
-
---
--- Indizes für die Tabelle `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`ProductID`);
-
---
--- Indizes für die Tabelle `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`RoleID`);
-
---
--- Indizes für die Tabelle `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`UserID`),
-  ADD KEY `RoleID` (`RoleID`);
-
---
--- Indizes für die Tabelle `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indizes für die Tabelle `users_groups`
---
-ALTER TABLE `users_groups`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
-  ADD KEY `fk_users_groups_users1_idx` (`user_id`),
-  ADD KEY `fk_users_groups_groups1_idx` (`group_id`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
+-- Constraints for dumped tables
 --
 
 --
--- AUTO_INCREMENT für Tabelle `activity`
---
-ALTER TABLE `activity`
-  MODIFY `ActivityID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `customer`
---
-ALTER TABLE `customer`
-  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `groups`
---
-ALTER TABLE `groups`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT für Tabelle `login_attempts`
---
-ALTER TABLE `login_attempts`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `order`
---
-ALTER TABLE `order`
-  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `orderline`
---
-ALTER TABLE `orderline`
-  MODIFY `OrderLineID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `orderstatus`
---
-ALTER TABLE `orderstatus`
-  MODIFY `OrderStatusID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `product`
---
-ALTER TABLE `product`
-  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT für Tabelle `role`
---
-ALTER TABLE `role`
-  MODIFY `RoleID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `user`
---
-ALTER TABLE `user`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT für Tabelle `users_groups`
---
-ALTER TABLE `users_groups`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `activity`
+-- Constraints for table `activity`
 --
 ALTER TABLE `activity`
   ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints der Tabelle `order`
+-- Constraints for table `order`
 --
 ALTER TABLE `order`
   ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`OrderStatusID`) REFERENCES `orderstatus` (`OrderStatusID`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -415,20 +291,20 @@ ALTER TABLE `order`
   ADD CONSTRAINT `order_ibfk_3` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints der Tabelle `orderline`
+-- Constraints for table `orderline`
 --
 ALTER TABLE `orderline`
   ADD CONSTRAINT `orderline_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `order` (`OrderID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `orderline_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints der Tabelle `user`
+-- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`RoleID`) REFERENCES `role` (`RoleID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints der Tabelle `users_groups`
+-- Constraints for table `users_groups`
 --
 ALTER TABLE `users_groups`
   ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
