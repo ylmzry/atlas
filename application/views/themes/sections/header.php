b034d270b53046843d4da3494c2fe9e14f8699ba
Eray YILMAZ <erayyilmaz@outlook.com>
2018-05-21 22:06:43 +0200

    sdasd
    asdas

--- application/views/themes/sections/header.php
+++ application/views/themes/sections/header.php
@@ -8,7 +8,7 @@
     <div class="brand inline">
       <img src="assets/img/logo.png" alt="logo" data-src="assets/img/logo.png" data-src-retina="assets/img/logo_2x.png" width="78" height="22">
     </div>
-    
+
     <a href="#" class="search-link hidden-md-down" data-toggle="search" style="display:none"><i class="pg-search"></i>Type anywhere to <span class="bold">search</span></a>
   </div>
   <div class="d-flex align-items-center">
@@ -145,10 +145,11 @@
 				<span class="text-master"><?php  echo $user["last_name"]; ?></span>
 			<?php } ?>
     </div>
+    <!--
     <div class="dropdown pull-right hidden-md-down">
       <button class="profile-dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="thumbnail-wrapper d32 circular inline">
-        <img src="<?php echo base_url() ?>template/pagesadmin/assets/img/profiles/avatar.jpg" alt="" data-src="<?php echo base_url() ?>template/pagesadmin/assets/img/profiles/avatar.jpg" data-src-retina="<?php echo base_url() ?>template/pagesadmin/assets/img/profiles/avatar.jpg" width="32" height="32">
+      <img src="<?php echo base_url() ?>template/pagesadmin/assets/img/profiles/avatar.jpg" alt="" data-src="<?php echo base_url() ?>template/pagesadmin/assets/img/profiles/avatar.jpg" data-src-retina="<?php echo base_url() ?>template/pagesadmin/assets/img/profiles/avatar.jpg" width="32" height="32">
         </span>
       </button>
       <div class="dropdown-menu dropdown-menu-right profile-dropdown" role="menu">
@@ -161,8 +162,9 @@
         </a>
       </div>
     </div>
+    -->
     <!-- END User Info-->
-    <a href="#" class="header-icon pg pg-alt_menu btn-link m-l-10 sm-no-margin d-inline-block" data-toggle="quickview" data-toggle-element="#quickview"></a>
+    <!--<a href="#" class="header-icon pg pg-alt_menu btn-link m-l-10 sm-no-margin d-inline-block" data-toggle="quickview" data-toggle-element="#quickview"></a>-->
   </div>
 </div>
 <!-- END HEADER -->
